88cc93d691f79e70ae60a20e6bc120be5189fc01
Rakesh <135061073+Y-Rakesh@users.noreply.github.com>
2025-09-02 19:10:02 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,7 +6,7 @@ export const generatetoken=(userId,res)=>{
   res.cookie("jwt",token,{
     maxAge:7*24*60*60*1000,//in ms
     httpOnly:true,//prevents xss cross scripting attacks
-    sameSite:"strict",//prevent csrf attacks
+    sameSite:"none",//prevent csrf attacks
     secure:process.env.NODE_ENV!=="development"
   })
 return token;
