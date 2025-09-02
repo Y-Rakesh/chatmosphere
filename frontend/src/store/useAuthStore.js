55e1f5e541430e82d176cab64f80634a66b1cf8c
Rakesh <135061073+Y-Rakesh@users.noreply.github.com>
2025-09-02 16:56:27 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,9 +2,10 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
-
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : import.meta.env.VITE_API_URL;
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
