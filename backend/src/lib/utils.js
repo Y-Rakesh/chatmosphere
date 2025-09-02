13dfee991d7c8dc4da0175fb4661a9ae51d7baae
Rakesh <135061073+Y-Rakesh@users.noreply.github.com>
2025-09-02 19:13:37 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generatetoken=(userId,res)=>{
     maxAge:7*24*60*60*1000,//in ms
     httpOnly:true,//prevents xss cross scripting attacks
     sameSite:"none",//prevent csrf attacks
-    secure:process.env.NODE_ENV!=="development"
+     secure: true
   })
 return token;
 
