473692d7b8f8810393c62a513cee5c0e4e100c8e
Rakesh <135061073+Y-Rakesh@users.noreply.github.com>
2025-09-02 17:22:24 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin:["https://chatmosphere-u7cy.onrender.com" ,"http://localhost:5173"], credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
